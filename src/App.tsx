e98cb1df75cca5e17dfc5b21ff2b582afd83196e
lycaan <emma.edlund@telia.com>
2025-09-21 00:09:45 +0200

    modified:   src/App.css 	modified:   src/App.tsx 	new file:   src/assets/cute-svgrepo-com.svg 	new file:   src/assets/fish-svgrepo-com.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/fish-svgrepo-com.svg'
+import viteLogo from './assets/cute-svgrepo-com.svg'
 import './App.css'
 
 function App() {
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Emma Edlund</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          That's me! This is my personal website
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This website has just been initialized. Please come back later to see further progress.
       </p>
     </>
   )
